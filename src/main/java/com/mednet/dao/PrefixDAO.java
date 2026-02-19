993064dfa3808b1a3b54389e45c33218c4be99d9
Nikita Sontakke <166376545+nikitasontakke2004@users.noreply.github.com>
2026-02-19 11:00:33 +0530

    Update PrefixDAO.java

--- src/main/java/com/mednet/dao/PrefixDAO.java
+++ src/main/java/com/mednet/dao/PrefixDAO.java
@@ -3,11 +3,20 @@ package com.mednet.dao;
 import com.mednet.model.Prefix;
 import com.mednet.util.HibernateUtil;
 import org.hibernate.*;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
+@Repository
 public class PrefixDAO {
+
+
+    private Session getSession() {
+        return HibernateUtil.getSessionFactory().openSession();
+    }
+
     public void save(Prefix p) {
-        Session s = HibernateUtil.getSessionFactory().openSession();
+        Session s = getSession(); // Called here
         Transaction tx = s.beginTransaction();
         s.saveOrUpdate(p);
         tx.commit();
@@ -15,14 +24,14 @@ public class PrefixDAO {
     }
 
     public List<Prefix> list() {
-        Session s = HibernateUtil.getSessionFactory().openSession();
+        Session s = getSession(); // Called here
         List<Prefix> list = s.createQuery("from Prefix").list();
         s.close();
         return list;
     }
 
     public void delete(int id) {
-        Session s = HibernateUtil.getSessionFactory().openSession();
+        Session s = getSession(); // Called here
         Transaction tx = s.beginTransaction();
         Prefix p = (Prefix) s.get(Prefix.class, id);
         if(p != null) s.delete(p);
